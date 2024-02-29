77cedcef860b8d8db5ab9526af446f4931ac02e6
Mustafa Imer <mustafaimer@gmail.com>
2024-02-29 17:42:55 +0300

    start

---
+++ app/src/main/java/com/imgego/android_kotlin_boilerplate/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.imgego.android_kotlin_boilerplate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.imgego.android_kotlin_boilerplate.ui.theme.AndroidKotlinBoilerplateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidKotlinBoilerplateTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidKotlinBoilerplateTheme {
+        Greeting("Android")
+    }
+}
